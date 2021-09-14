3c724b57444fa9e71457363e85536cd007e96ad3
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-09-14 13:32:03 -0500

    working controller and api
    Signed-off-by: Jose R. Gonzalez <josegonzalez89@gmail.com>

--- controllers/demoresource_controller.go
+++ controllers/demoresource_controller.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"context"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -39,18 +40,30 @@ type DemoResourceReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DemoResource object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *DemoResourceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	key := req.NamespacedName
+	l := log.FromContext(ctx).WithValues("NamespacedName", key)
 
-	// your logic here
+	l.Info("This is the reconciliation loop of the simple-demo-operator.")
 
+	var rsrc toolsv1.DemoResource
+	if err := r.Get(ctx, key, &rsrc); err != nil {
+		if apierrors.IsNotFound(err) {
+			l.Info("Whoops! We couldn't find the resource associated with this event.")
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{}, err
+	}
+
+	rsrc.Status.SpecMessage = rsrc.Spec.Message
+	l.Info("Let's update the status with the message from the spec!")
+	if err := r.Status().Update(ctx, &rsrc, &client.UpdateOptions{}); err != nil {
+		l.Info("Uh oh! I ran into something unexpected.")
+		return ctrl.Result{}, err
+	}
+
+	l.Info("All finished!")
 	return ctrl.Result{}, nil
 }
 
